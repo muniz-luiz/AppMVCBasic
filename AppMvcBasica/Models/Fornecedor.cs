616e3a11fc5bbd7282c1f611aaf6c20cf1cc4e91
Luiz Muniz <eusouluizeduardomuniz@gmail.com>
2023-04-03 09:36:12 -0300

    Aplicação modelo e exemplo criada para cadastrar fornecedor e produto

---
+++ AppMvcBasica/Models/Fornecedor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppMvcBasica.Models
+{
+    public class Fornecedor : Entity
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+
+        [DisplayName("Ativo?")]
+        public bool Ativo { get; set; }
+
+        /* EF Relations */
+        public IEnumerable<Produto> Produtos { get; set; }
+
+    }
+}
---
+++ AppMvcBasica/Program.cs
@@ -0,0 +1,59 @@
+﻿using AppMvcBasica.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region " Configurando serviços no container "
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+#endregion
+
+# region " Configurando o resquest dos serviços no pipeline "
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+// Adicionando suporte a rota
+app.UseRouting();
+
+// Autenticacao e autorização (Identity)
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Rota padrão
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+// Mapeando componentes Razor Pages (ex: Identity)
+app.MapRazorPages();
+
+app.Run();
+
+#endregion
